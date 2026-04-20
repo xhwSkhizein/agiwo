05bec8b41c531006f23ce2b5a283dec6fcb1ac80
xuhongwei <xuhongwei@weimiao.cn>
2026-04-20 09:41:06 +0800

    Optimize console Docker build

--- console/web/next.config.ts
+++ console/web/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["127.0.0.1", "10.0.18.131", "localhost"],
+  output: "standalone",
 };
 
 export default nextConfig;
